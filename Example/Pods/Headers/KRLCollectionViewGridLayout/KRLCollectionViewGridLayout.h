11779490b579615386b60858747e26111a2a69bb
Kevin Lundberg <kevinrlundberg@gmail.com>
2014-07-07 00:51:32 -0400

    removed pods directory, others can manually pod install

--- Example/Pods/Headers/KRLCollectionViewGridLayout/KRLCollectionViewGridLayout.h
+++
@@ -1,1 +0,0 @@
-../../../../Classes/KRLCollectionViewGridLayout.h
--- Example/Pods/Pods-KRLCollectionViewGridLayoutTests-KRLCollectionViewGridLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KRLCollectionViewGridLayoutTests_KRLCollectionViewGridLayout : NSObject
-@end
-@implementation PodsDummy_Pods_KRLCollectionViewGridLayoutTests_KRLCollectionViewGridLayout
-@end
